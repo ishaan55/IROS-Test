0692b2dd1324fa3c8befd4a1b5e259302893f02b
Ishaan Gupta <derpboy77@Ishaans-MacBook-Air.local>
2023-08-18 15:04:17 +0530

    move only foward

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,37 +1,35 @@
-# Copyright 1996-2023 Cyberbotics Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Minimalist controller example for the Robot Wrestling Tournament.
    Demonstrates how to play a simple motion file."""
 
 from controller import Robot
 import sys
 
-# We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
-# If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
 from utils.motion_library import MotionLibrary
+from utils.image_processing import ImageProcessing as IP
+from utils.fall_detection import FallDetection
+# from utils.gait_manager import GaitManager
+from utils.camera import Camera
 
 
 class Wrestler (Robot):
+    def __init__(self):
+        Robot.__init__(self)
+        self.time_step = int(self.getBasicTimeStep())
+
+        self.camera = Camera(self)
+        self.fall_detector = FallDetection(self.time_step, self)
+        # self.gait_manager = GaitManager(self, self.time_step)
+        self.heading_angle = 3.14 / 2
+        # Time before changing direction to stop the robot from falling off the ring
+        self.counter = 0
+
     def run(self):
         # to load all the motions from the motions folder, we use the MotionLibrary class:
         motion_library = MotionLibrary()
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            motion_library.play('Backwards')
+        while self.step(self.time_step) != -1:  # mandatory function to make the simulation run
+            motion_library.play('Forwards')
 
 
 # create the Robot instance and run main loop
